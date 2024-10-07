c52388bc1328c0a36cb50b09fbee65e5e20b98be
Yaakovben <yaakovbenyizchak1@gmail.com>
2024-10-07 15:25:17 +0300

    connected to mongoDB

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const exp = require('express')
 const dotenv = require('dotenv')
+const {connectToMongo} = require('./config/dbConfig')
 
 const app = exp()
 dotenv.config()
+connectToMongo()
 const port = process.env.PORT ||1010;
 
-app.use(exp.json())
+app.use(exp.json())    
 app.use('/auth', require('./routes/authRouter'))                  
 app.use('/user', require('./routes/userRouter'))
 app.use('/greenEye', require('./routes/greenEyeRouter'))
